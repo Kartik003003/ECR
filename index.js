aff0c17f27eee80e70576fb50a49c77a4e28dcae
Kartikeya singh Thakur <100180513+Kartik003003@users.noreply.github.com>
2024-05-18 20:41:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my app");
+});
+
+app.get("/info", (req, res) => {
+  res.send("hey this is /info api");
+});
+
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
